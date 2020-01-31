cff757c9689b23bb2d9e8d54dd0a61abfa5d1ba5
Ilya Zverev <izverev@logrocon.com>
2020-01-31 11:57:16 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# choco-marp-cli
+Chocolatey package for marp-cli https://github.com/marp-team/marp-cli/
---
+++ marp-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/marp-team/marp-cli/releases/download/v0.17.0/marp-cli-v0.17.0-win.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'marp-cli*'
+  checksum      = '20802f86d54ef81acb5948f48fdac07bd6e529068569d6758a4f139e89cab617'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
