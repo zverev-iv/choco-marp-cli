f6ed9708a23cec8e26b14e3e77cc51e294ba798e
zverev-iv <51955637+zverev-iv@users.noreply.github.com>
2020-07-23 11:43:02 +0300

    Update chocolateyinstall.ps1

--- marp-cli/tools/chocolateyinstall.ps1
+++ marp-cli/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/marp-team/marp-cli/releases/download/v0.18.2/marp-cli-v0.18.2-win.zip'
+$url        = 'https://github.com/marp-team/marp-cli/releases/download/v0.18.3/marp-cli-v0.18.3-win.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   softwareName  = 'marp-cli*'
-  checksum      = 'fe2dffaeaeeb971acbc8f8db39f07085d26099c3aaad3c599445ef3595a19be2'
+  checksum      = '1477bf86dc7ab37a4af5a169d0baec9b843d8b3dd1441defefd52e80ab8c90e3'
   checksumType  = 'sha256'
 }
 
