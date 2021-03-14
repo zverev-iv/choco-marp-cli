3659e8aeec992f3decb7ebe5982a11f408a1550c
zverev iv <mail@zverev-iv.me>
2021-03-14 19:49:37 +0300

    up to 0.23.1

--- marp-cli/tools/chocolateyinstall.ps1
+++ marp-cli/tools/chocolateyinstall.ps1
@@ -1,14 +1,12 @@
 $ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/marp-team/marp-cli/releases/download/v0.23.0/marp-cli-v0.23.0-win.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = $url
-  softwareName  = 'marp-cli*'
-  checksum      = 'F958782184F216240D26917E3737036025E00A6E0744C6E67E6D9F5EE5E35960'
-  checksumType  = 'sha256'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  url           = "https://github.com/marp-team/marp-cli/releases/download/v0.23.1/marp-cli-v0.23.1-win.zip"
+  softwareName  = "marp-cli*"
+  checksum      = "A26623A3FB608C00482F57BB7AA294968D128890EB0564DE9EC8EA06B5743AB0"
+  checksumType  = "sha256"
 }
 
 Install-ChocolateyZipPackage @packageArgs
